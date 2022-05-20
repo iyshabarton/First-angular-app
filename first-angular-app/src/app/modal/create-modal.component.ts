b4f374d6ff01a024785128a44f4049856d44da15
Iysha Barton <iyshabarton@MacBook-Pro-de-Iysha.local>
2022-05-20 17:11:59 +0200

    modal in place

--- first-angular-app/src/app/modal/create-modal.component.ts
+++ first-angular-app/src/app/modal/create-modal.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { NgbActiveModal, NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-create-modal',
@@ -7,14 +8,30 @@ import { NgbActiveModal, NgbModal } from '@ng-bootstrap/ng-bootstrap';
   styleUrls: ['./create-modal.component.css'],
 })
 export class CreateModalComponent implements OnInit {
+  profileForm = new FormGroup({
+    name: new FormControl(''),
+    description: new FormControl(''),
+    imageUrl: new FormControl(''),
+    price: new FormControl(''),
+  });
   constructor(public activeModal: NgbActiveModal) {}
 
   ngOnInit(): void {}
-  save(name: string): void {
-    const normalizedName = this.hello(name);
-    this.activeModal.close(normalizedName);
+  save(
+    name: string,
+    description: string,
+    imageUrl: string,
+    price: string
+  ): void {
+    this.activeModal.close(name);
+    console.warn(this.profileForm.value);
   }
-  private hello(name: string): string {
-    return `hello ${name}`;
+  private hello(
+    name: string,
+    description: string,
+    imageUrl: string,
+    price: string
+  ): string {
+    return `hello ${name}, ${description}, ${imageUrl}, ${price}`;
   }
 }
